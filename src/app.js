14dea9da927bba6c751e45904e88ce5d77b8267b
souma9830 <prabirkumarshee8@gmail.com>
2026-03-25 12:40:42 +0530

    Add Product Route in server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.routes.js');
 const wasteLogRouter = require('./routes/wasteLog.route.js');
 const adminRouter = require('./routes/admin.route.js');
+const productRouter = require('./routes/product.routes.js');
 const app = express();
 
 app.use(cors());
@@ -13,4 +14,5 @@ app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/iot', wasteLogRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/products', productRouter);
 module.exports = app;
