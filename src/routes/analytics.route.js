f22b979c1cfbf0595227213add8ffa36c17b3cbc
Subham777-max <subhamray865@gmail.com>
2026-03-25 19:44:13 +0530

    analytics api created

---
+++ src/routes/analytics.route.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { authUser } = require('../middleware/auth.middleware.js');
+const { getCollegeWasteLast7Days,getUserWasteLast7Days,getUserWasteById } = require('../controller/analytics.controller.js');
+
+/**
+ * @route GET /api/analytics/weekly/college
+ * @desc Get weekly analytics data for waste collection (overall college)
+ * @access Private (Admin only)
+ */
+router.get("/weekly/college",authUser,getCollegeWasteLast7Days);
+
+/**
+ * @route GET /api/analytics/weekly/my
+ * @desc Get weekly analytics data for waste collection (individual student(current user))
+ * @access Private (Authenticated users only)
+ */
+router.get("/weekly/my",authUser,getUserWasteLast7Days);
+
+/**
+ * @route GET /api/analytics/weekly/user/:id
+ * @desc Get weekly analytics data for waste collection (individual student by id)
+ * @access Private (Admin only)
+ */
+router.get("/weekly/user/:id",authUser,getUserWasteById);
+module.exports = router;
