b9677a447b08691907ead8cdbdaa56dbd443258a
souma9830 <prabirkumarshee8@gmail.com>
2026-03-26 09:28:57 +0530

    Chnages

--- src/utils/rewardCalculator.js
+++ src/utils/rewardCalculator.js
@@ -1,5 +1,6 @@
 function calculateReward(weight) {
   // Input 'weight' is expected to be in GRAMS (since we've already converted KG to Grams in the controller call)
+  if (weight >= 1 && weight < 10) return 2;
   if (weight >= 10 && weight < 20) return 10;
   if (weight >= 20 && weight < 50) return 20;
   if (weight >= 50 && weight < 100) return 40;
