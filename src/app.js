6898a98a49374a1805c7f3b7869bc3e76164cc10
souma9830 <prabirkumarshee8@gmail.com>
2026-08-08 22:20:23 +0530

    Deployed

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const vendorRouter = require('./routes/vendor.route.js');
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://camera-model-rho.vercel.app"],
+    origin: ["http://localhost:5173", "https://camera-model-rho.vercel.app", "https://green-coin-frontend.vercel.app/"],
     credentials: true,
 }));
 app.use(cookieParser());
